90b3cfe770a6d0b230aeeaf3efcb123ceecc1dce
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2023-10-14 21:19:42 +0900

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const path = require('path')
+const AutoLoad = require('@fastify/autoload')
+
+// Pass --options via CLI arguments in command to enable these options.
+module.exports.options = {}
+
+module.exports = async function (fastify, opts) {
+  // Place here your custom code!
+
+  // Do not touch the following lines
+
+  // This loads all plugins defined in plugins
+  // those should be support plugins that are reused
+  // through your application
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'plugins'),
+    options: Object.assign({}, opts)
+  })
+
+  // This loads all plugins defined in routes
+  // define your routes in one of these
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'routes'),
+    options: Object.assign({}, opts)
+  })
+
+  fastify.register(require('@fastify/cors'));
+
+  fastify.post('/getData', async (request, reply) => {
+    // ここに外部APIへのリクエストを書く
+  
+    // 引数のreplyを使ってWeb側へデータを渡す
+    reply.send({message: 'Hello World!'});
+  });
+}
