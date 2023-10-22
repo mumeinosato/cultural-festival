d27a8babf59d92c49c57c67769b55a66da4e2f1f
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2023-10-22 19:14:02 +0900

    footer

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer>
+        <div>
+            <p class="footer-text">
+                制作:〇〇<br>
+                お問い合わせは<a href="https://example.com" target="_blank">こちら</a><br>
+                &copy;〇〇
+            </p>
+        </div>
+    </footer>
+</template>
